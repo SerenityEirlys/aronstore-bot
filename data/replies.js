89a83280a98ff2e52918fdf84396ab3ff4883d0e
Eirlys <100836558+SerenityEirlys@users.noreply.github.com>
2025-01-19 16:20:05 +0700

    Add files via upload

---
+++ data/replies.js
@@ -0,0 +1,19 @@
+// Danh sách các từ khóa và câu trả lời tương ứng
+const replies = {
+    // Chào hỏi
+    'hello': ['Xin chào!'],
+    'hi': ['Xin chào!'],
+    'chào': ['Xin chào bạn!'],
+
+    // Cảm ơn
+    'cảm ơn': ['Không có gì!'],
+    'thanks': ['You\'re welcome!'],
+
+    // Tạm biệt
+    'ngu': ['mẹ mày béo'],
+    'tạm biệt': ['Tạm biệt bạn!'],
+
+};
+
+// Export để sử dụng ở các file khác
+module.exports = replies;
