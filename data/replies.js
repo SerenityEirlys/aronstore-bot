3221dbd013920aa4723418cee1c3a66f7419013d
tuanloc1105 <tuanloc1105@gmail.com>
2025-02-03 16:58:33 +0700

    update: format code and change the logic

--- data/replies.js
+++ data/replies.js
@@ -1,38 +1,36 @@
 // Danh sách các từ khóa và câu trả lời tương ứng
 const replies = {
     // Download TP
-    'custom tp': ['You can check https://discord.com/channels/1104940962804936856/1231627318380859514!'],
-    'custom tp': ['Bạn có thể kiểm tra ở đây https://discord.com/channels/1104940962804936856/1231627318380859514!'],
-    'tp': ['Bạn có thể kiểm tra ở đây https://discord.com/channels/1104940962804936856/1231627318380859514!' ],
-    'tp': ['You can check https://discord.com/channels/1104940962804936856/1231627318380859514!' ],
+    'custom tp': ['You can check https://discord.com/channels/1104940962804936856/1231627318380859514!', 'Bạn có thể kiểm tra ở đây https://discord.com/channels/1104940962804936856/1231627318380859514!'],
+    'tp': ['Bạn có thể kiểm tra ở đây https://discord.com/channels/1104940962804936856/1231627318380859514!', 'You can check https://discord.com/channels/1104940962804936856/1231627318380859514!'],
     
     // Tutorial Hướng Dẫn
     'instruct': ['You can see at https://docs.arons.wiki/'],
-    'hướng dẫn': ['bạn có thể xem tại https://docs.arons.wiki/vietnamese'],
-   
+    'huong dan': ['bạn có thể xem tại https://docs.arons.wiki/vietnamese'],
+
     // Download Loader
     'download loader': ['you can load the loader via this command /download ( select your game you play)!'],
-    'tải loader': ['Bạn có thể tải loader qua lệnh /download ( chọn game bạn chơi)!'],
+    'tai loader': ['Bạn có thể tải loader qua lệnh /download ( chọn game bạn chơi)!'],
 
-    
+
     // Xem Video ở đâu 
     'video': ['You can watch the video here https://www.youtube.com/@aronslie'],
     'xem video': ['bạn có thể xem video ở đây https://www.youtube.com/@aronslie '],
-    'có video': ['bạn có thể xem video ở đây https://www.youtube.com/@aronslie '],
-    
+    'co video': ['bạn có thể xem video ở đây https://www.youtube.com/@aronslie '],
+
     // Mua ở đâu
     'buy': ['You can buy here https://arons.mysellix.io/ or https://arons.store/'],
     'mua': ['Bạn có thể mua ở https://arons.dev/'],
-    
+
     // Where Products
     'how to get my license': ['If you buy on sellix check Gmail , Spawn Folder If you buy on arons.store check Order'],
     'how to get key': ['If you buy on sellix check Gmail , Spawn Folder If you buy on arons.store check Order'],
-    
+
     // Tạm biệt
     'bye': ['Tạm biệt!'],
-    'tạm biệt': ['Tạm biệt bạn!'],
+    'tam biet': ['Tạm biệt bạn!'],
 
-    
+
 };
 
 // Export để sử dụng ở các file khác
